306d5dab0e440867d01ef6a636ceb5ae38aeac74
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-11-22 01:12:42 +0300

    data-layer/unitOfWork class added

---
+++ AuthServer.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+}
